936940532becb79c97296c29aa89f7ce86927151
Edgar Solovev <edgar@MacBook-Pro-Edgar.local>
2024-01-29 21:56:26 +0300

    refactor module

---
+++ src/modules/esm.mjs
@@ -0,0 +1,43 @@
+import path from 'path';
+import {release, version} from 'os';
+import {createServer as createServerHttp} from 'http';
+import './files/c.js';
+
+const random = Math.random();
+
+let unknownObject;
+const options = {
+    assert: {
+        type: "json",
+    },
+};
+
+if (random > 0.5) {
+    unknownObject = import('./files/a.json', options);
+} else {
+    unknownObject = import('./files/b.json', options);
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${import.meta.url}`);
+console.log(`Path to current directory is ${path.dirname(import.meta.url)}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+unknownObject.then(data => {
+    console.log(data);
+});
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {unknownObject, myServer};
